63f38b0aae87331b5f759cbb4d5407657c7622de
rajusiripalli <rajusiripalli7@gmail.com>
2025-11-29 13:51:07 +0530

    expo router setup 27 mins finsihed

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function ProfileScreen() {
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      <Text>ProfileScreenn</Text>
     </View>
   );
 }
