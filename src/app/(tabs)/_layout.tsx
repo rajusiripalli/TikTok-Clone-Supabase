920778c76d1e85de92a1b27718173064b5210f3d
rajusiripalli <rajusiripalli7@gmail.com>
2025-11-29 13:50:08 +0530

    expo router setup finished

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,60 @@
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: "Friends",
+          tabBarIcon: ({ color }) => (
+            <Feather name="users" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="newPost"
+        options={{
+          title: "New Post",
+          tabBarIcon: ({ color }) => (
+            <Feather name="plus-square" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="message-minus-outline"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/(tabs)/newPost.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NewPostScreen() {
+  return (
+    <View>
+      <Text>NewPostScreen</Text>
+    </View>
+  );
+}
