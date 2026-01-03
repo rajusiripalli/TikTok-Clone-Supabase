42696a2d404fa421f44ff1d779f223e08d165850
rajusiripalli <rajusiripalli7@gmail.com>
2026-01-03 23:13:02 +0530

    53mins building home screen feed

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -18,7 +18,7 @@ export default function PostListItem() {
     player.play();
   });
   return (
-    <View style={{ height }}>
+    <View style={{ height: height - 80 }}>
       <VideoView
         style={{ flex: 1 }}
         player={player}
@@ -26,39 +26,43 @@ export default function PostListItem() {
         nativeControls={false}
       />
 
-      <View>
+      <View style={styles.interactionBar}>
         <TouchableOpacity
-          style={{}}
+          style={styles.interactionButton}
           onPress={() => console.log("Like Pressed")}
         >
           <Ionicons name="heart" size={33} color="#fff" />
-          <Text>0</Text>
+          <Text style={styles.interactionTxt}>0</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{}}
+          style={styles.interactionButton}
           onPress={() => console.log("comment Pressed")}
         >
           <Ionicons name="chatbubble" size={33} color="#fff" />
-          <Text>0</Text>
+          <Text style={styles.interactionTxt}>0</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{}}
+          style={styles.interactionButton}
           onPress={() => console.log("share Pressed")}
         >
           <Ionicons name="arrow-redo" size={33} color="#fff" />
-          <Text>20</Text>
+          <Text style={styles.interactionTxt}>20</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{}}
+          style={styles.avatar}
           onPress={() => console.log("profile Pressed")}
         >
           <View>
-            <Text>L</Text>
+            <Text style={styles.avatarTxt}>L</Text>
           </View>
         </TouchableOpacity>
+      </View>
+      <View style={styles.videoInfo}>
+        <Text style={styles.username}>Jesus</Text>
+        <Text style={styles.desscription}>Jesus Loves You</Text>
       </View>
     </View>
   );
@@ -67,5 +71,44 @@ export default function PostListItem() {
 const styles = StyleSheet.create({
   interactionBar: {
     position: "absolute",
+    right: 20,
+    bottom: 20,
+    alignItems: "center",
+    gap: 25,
+  },
+  interactionButton: {
+    alignItems: "center",
+  },
+  interactionTxt: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "600",
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  avatarTxt: {
+    fontSize: 25,
+  },
+  videoInfo: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 100,
+    gap: 5,
+  },
+  username: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  desscription: {
+    color: "#fff",
+    fontSize: 14,
   },
 });
